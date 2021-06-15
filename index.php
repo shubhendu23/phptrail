3aed0d65cba5e11e4123fc553260841ebe9e7849
shubhendu23 <64519002+shubhendu23@users.noreply.github.com>
2021-06-15 20:02:34 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <pre>
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'src/Exception.php';
+require 'src/PHPMailer.php';
+require 'src/SMTP.php';
 
         print ("Checking for libraries.");
         print `date`;
