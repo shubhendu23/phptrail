d2b4b4eaa8852d7b9f7d11c0f39a67192baae391
shubhendu23 <64519002+shubhendu23@users.noreply.github.com>
2021-06-15 20:24:20 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <pre>
 <?php
-
+ print ("Checking for libraries1st pass.");
+  
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+  print ("Checking for libraries 2nd pass.");
+require 'vendor/autoload.php';
 
         print ("Checking for libraries.");
         print `date`;
